8211be74de5eb8db7c98da9b05620d380d13702f
Tonko_wq55 <awdawd@gaawd>
2025-02-26 09:46:38 +0200

    v2

--- main.py
+++ main.py
@@ -34,17 +34,34 @@ def increase_brush_size():
     if brush_size < 20:
         brush_size += 1
 
-def color_change():
-    pass
+
+def color_change(new_color):
+    global color
+    color = new_color
+
 
 root = Tk()
 root.title("Paint")
 
-w = Canvas(root, width=c.WIDTH, highlightcolor=c.HEIGHT, bg=c.BG_COLOR)
+w = Canvas(root, width=c.WIDTH, height=c.HEIGHT, bg=c.BG_COLOR)
 eraser_icon = load_image('eraser.png')
 increase_icon = load_image('inbrush.png')
-decrease_icon = load_image('dedrush.png')
-brush_icon = load_image('drush.png')
-decrease_dtn = Button(root, image=decrease_icon, compound=decrease_brush_size)
-increase_dtn = Button(root, image=increase_icon, compound=increase_brush_size)
-brush_dtn = Button(root, image=increase_icon, compound=lambda : color_change())
+decrease_icon = load_image('debrush.png')
+brush_icon = load_image('brush.png')
+decrease_btn = Button(image=decrease_icon, command=decrease_brush_size)
+increase_btn = Button(image=increase_icon, command=increase_brush_size)
+brush_btn = Button(image=brush_icon, command=lambda: color_change(c.DEFAULT_COLOR))
+remove_all_btn = Button(text="Remove all", width=10, command=lambda: w.delete("all"))
+
+blanched_almond_btn = Button(bg="#FFF1C9", width=2, command=lambda: color_change("#FFF1C9"))
+melon_btn = Button(bg="#F7B7A3", width=2, command=lambda: color_change("#F7B7A3"))
+water_mellon_btn = Button(bg="#EASF89", width=2, command=lambda: color_change("#EASF89"))
+violet_btn = Button(bg="#9B3192", width=2, command=lambda: color_change("#9B3192"))
+dark_purple_btn = Button(bg="#2B0B3F", width=2, command=lambda: color_change("#2B0B3F"))
+alien_armpit_btn = Button(bg='#89DF00', width=2, command=lambda: color_change('#89DF00'))
+spring_bud_btn = Button(bg='#A1F500', width=2, command=lambda: color_change('#A1F500'))
+magic_mint_btn = Button(bg='#ADFFB9', width=2, command=lambda: color_change('#ADFFB9'))
+medium_spring_green_btn = Button(bg='#00F798', width=2, command=lambda: color_change('#00F798'))
+green_btn = Button(bg='#10B56F', width=2, command=lambda: color_change('#10B56F'))
+
+mainloop()
